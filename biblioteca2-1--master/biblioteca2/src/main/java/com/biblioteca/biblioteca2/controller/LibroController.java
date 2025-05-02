30883cf520c5ccf33042f580ac3ed116bd14bab4
Edgard <edga.hernandez@duocuc.cl>
2025-05-02 14:02:41 -0400

    real

--- biblioteca2-1--master/biblioteca2/src/main/java/com/biblioteca/biblioteca2/controller/LibroController.java
+++ biblioteca2-1--master/biblioteca2/src/main/java/com/biblioteca/biblioteca2/controller/LibroController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
-@RestController //Clase de tipooo controlador rest
+@RestController //Clase de tipppooo controlador rest
 @RequestMapping("/libros") //para indicarle la ruta base de mi proyecto
 
 public class LibroController {
